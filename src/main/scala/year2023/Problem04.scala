e8fbc9e636abc04daa341957147f3f9ff3daa85c
Ricardo Martins <ricardocmartins91@gmail.com>
2023-12-07 22:16:03 +0000

    Added 2nd part of problem04

--- src/main/scala/year2023/Problem04.scala
+++ src/main/scala/year2023/Problem04.scala
@@ -2,6 +2,8 @@ package year2023
 
 import better.files.File
 
+import scala.annotation.tailrec
+
 object Problem04 {
 
   def main(args: Array[String]): Unit = {
@@ -19,7 +21,35 @@ object Problem04 {
         points(filteredCards.size)
       }
 
-    println(pointsSeq.sum)
+    println("First Part: " + pointsSeq.sum)
+
+    val amountOfMatchesSeq: Seq[Int] =
+      gamesLines.map { gameStr =>
+        val Seq(winningCardsStr, myCardsStr) = gameStr.split("\\|").toSeq.map(_.trim)
+        val winningCards = winningCardsStr.split(" ").toSeq.filter(_.nonEmpty).map(_.toInt)
+        val myCards = myCardsStr.split(" ").toSeq.filter(_.nonEmpty).map(_.toInt)
+        val filteredCards = myCards.filter(winningCards.contains)
+        filteredCards.size
+      }
+
+    @tailrec
+    def loop(seq: Seq[(Int, Int)], index: Int): Seq[(Int, Int)] = {
+      if (index >= seq.size)
+        seq
+      else {
+        val (amount, matches) = seq(index)
+        val newSeq: Seq[(Int, Int)] =
+          (index + 1 to index + matches).foldLeft(seq) { case (acc, i) =>
+            val (oldAmount, oldMatches) = acc(i)
+            acc.updated(i, (oldAmount + amount, oldMatches))
+          }
+        loop(newSeq, index + 1)
+      }
+    }
+
+    println(
+      "Second Part: " + loop(amountOfMatchesSeq.map((1, _)), 0).map(_._1).sum
+    )
   }
 
 }
