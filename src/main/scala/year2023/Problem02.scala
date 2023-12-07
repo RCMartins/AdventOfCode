7d674957b39c0dfbac8877961a30265ee8f39b3c
Ricardo Martins <ricardocmartins91@gmail.com>
2023-12-07 21:50:08 +0000

    Added 2nd part of problem02

--- src/main/scala/year2023/Problem02.scala
+++ src/main/scala/year2023/Problem02.scala
@@ -27,7 +27,28 @@ object Problem02 {
         val isValid: Boolean = setsValid.forall(identity)
         Some(index + 1).filter(_ => isValid)
       }
-    println(ids.sum)
+    println("First Part: " + ids.sum)
+
+    val powerSeq: Seq[Int] =
+      input.lines.toSeq.map { line =>
+        val gameContent = line.dropWhile(_ != ':').drop(1).trim
+        val setsStrSeq = gameContent.split(";").toSeq.map(_.trim)
+        setsStrSeq
+          .foldLeft((0, 0, 0)) { case ((a, b, c), set) =>
+            set
+              .split(",")
+              .toSeq
+              .map(_.trim.split(" ").toSeq)
+              .map { case Seq(amountStr, color) => (amountStr.toInt, color) }
+              .foldLeft((a, b, c)) {
+                case ((a, b, c), (amount, "red"))   => (Math.max(a, amount), b, c)
+                case ((a, b, c), (amount, "green")) => (a, Math.max(b, amount), c)
+                case ((a, b, c), (amount, "blue"))  => (a, b, Math.max(c, amount))
+              }
+          }
+          .pipe { case (a, b, c) => a * b * c }
+      }
+    println("Second Part: " + powerSeq.sum)
   }
 
 }
