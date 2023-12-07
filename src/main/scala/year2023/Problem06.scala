ec1c139cadc880e7cc4456671fc312e7ae41bd76
Ricardo Martins <ricardocmartins91@gmail.com>
2023-12-07 22:27:50 +0000

    Added 2nd part of problem06

--- src/main/scala/year2023/Problem06.scala
+++ src/main/scala/year2023/Problem06.scala
@@ -9,20 +9,32 @@ object Problem06 {
   def main(args: Array[String]): Unit = {
     val input = File(getClass.getResource("/year2023/problem06.in"))
     val Seq(timeLine, distLine) = input.lines.toSeq
-    val times: Seq[Int] =
+    val timesSeq: Seq[Int] =
       timeLine.stripPrefix("Time:").split(" ").filter(_.nonEmpty).map(_.toInt).toSeq
-    val dist: Seq[Int] =
+    val distSeq: Seq[Int] =
       distLine.stripPrefix("Distance:").split(" ").filter(_.nonEmpty).map(_.toInt).toSeq
 
     val solutions: Seq[Int] =
-      times.zip(dist).map { case (time, dist) =>
+      timesSeq.zip(distSeq).map { case (time, dist) =>
         (1 until time).count { timeHoldDown =>
           (time - timeHoldDown) * timeHoldDown >= dist
         }
       }
 
     println(
-      solutions.product
+      "First Part: " + solutions.product
+    )
+
+    val solutions2 = {
+      val time = timesSeq.map(_.toString).mkString.toLong
+      val dist = distSeq.map(_.toString).mkString.toLong
+      (1L until time).count { timeHoldDown =>
+        (time - timeHoldDown) * timeHoldDown >= dist
+      }
+    }
+
+    println(
+      "Second Part: " + solutions2
     )
   }
 
