43c1e561c1b61e1f7ff573110e76473b3638a5fb
RCMartins <ricardocmartins91@gmail.com>
2023-12-09 20:38:57 +0000

    Added problem 02 of 2022 (first and second parts)

---
+++ src/main/scala/year2022/Problem02.scala
@@ -0,0 +1,46 @@
+package year2022
+
+import better.files.File
+
+object Problem02 {
+
+  def main(args: Array[String]): Unit = {
+    val input = File(getClass.getResource("/year2022/problem02.in"))
+    val lines = input.lines.toSeq
+
+    val result1 =
+      lines.map { case s"$opp $my" =>
+        (opp, my) match {
+          case ("A", "X") => 4
+          case ("B", "X") => 1
+          case ("C", "X") => 7
+          case ("A", "Y") => 8
+          case ("B", "Y") => 5
+          case ("C", "Y") => 2
+          case ("A", "Z") => 3
+          case ("B", "Z") => 9
+          case ("C", "Z") => 6
+        }
+      }
+
+    println("First Part: " + result1.sum)
+
+    val result2 =
+      lines.map { case s"$opp $my" =>
+        (opp, my) match {
+          case ("A", "X") => 3
+          case ("B", "X") => 1
+          case ("C", "X") => 2
+          case ("A", "Y") => 4
+          case ("B", "Y") => 5
+          case ("C", "Y") => 6
+          case ("A", "Z") => 8
+          case ("B", "Z") => 9
+          case ("C", "Z") => 7
+        }
+      }
+
+    println("Second Part: " + result2.sum)
+  }
+
+}
