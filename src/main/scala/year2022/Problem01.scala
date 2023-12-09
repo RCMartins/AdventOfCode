ce1367e45f3b58bc2e32805352ebbd111155e58c
RCMartins <ricardocmartins91@gmail.com>
2023-12-09 20:16:43 +0000

    Added problem 01 of 2022 (first and second parts)

---
+++ src/main/scala/year2022/Problem01.scala
@@ -0,0 +1,28 @@
+package year2022
+
+import better.files.File
+
+import scala.util.chaining.scalaUtilChainingOps
+
+object Problem01 {
+
+  def main(args: Array[String]): Unit = {
+    val input = File(getClass.getResource("/year2022/problem01.in"))
+    val lines = input.lines.toSeq :+ ""
+
+    def getTopX(topX: Int): Int =
+      lines
+        .foldLeft((Seq.empty[Int], 0)) {
+          case ((record, group), "") =>
+            ((record :+ group).sortBy(-_).take(topX), 0)
+          case ((record, group), line) =>
+            (record, group + line.toInt)
+        }
+        ._1
+        .sum
+
+    println("First Part: " + getTopX(1))
+    println("Second Part: " + getTopX(3))
+  }
+
+}
