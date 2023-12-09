2d1cd3a1af76fbb26a2039a35fd5c80283619429
RCMartins <ricardocmartins91@gmail.com>
2023-12-09 23:01:25 +0000

    Added problem 06 of 2022 (first and second parts)

---
+++ src/main/scala/year2022/Problem06.scala
@@ -0,0 +1,26 @@
+package year2022
+
+import better.files.File
+
+object Problem06 {
+
+  def main(args: Array[String]): Unit = {
+    val input = File(getClass.getResource("/year2022/problem06.in"))
+    val lines = input.lines.toSeq
+
+    def getUniqueWindow(size: Int): Int =
+      lines.head
+        .sliding(size)
+        .zipWithIndex
+        .find { case (window, _) =>
+          window.distinct.length == size
+        }
+        .get
+        ._2 + size
+
+    println("First Part: " + getUniqueWindow(4))
+
+    println("Second Part: " + getUniqueWindow(14))
+  }
+
+}
