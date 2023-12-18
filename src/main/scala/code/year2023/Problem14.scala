5def797323df0e2f317fe79dd2cc896b115c9ff2
Ricardo Martins <ricardocmartins91@gmail.com>
2023-12-18 19:18:37 +0000

    WIP

--- src/main/scala/code/year2023/Problem14.scala
+++ src/main/scala/code/year2023/Problem14.scala
@@ -9,9 +9,55 @@ object Problem14 {
 
   def main(args: Array[String]): Unit = {
     val input = File(getClass.getResource("/year2023/problem14.in"))
-    val columns = input.lines.toList.transpose
+//    val columns = input.lines.toList.transpose
+//
+//    val result1: List[Int] =
+//      columns.map { column =>
+//        val colIndexed = column.zipWithIndex
+//        val walls = colIndexed.filter(_._1 == '#').map(_._2)
+//        val rocks = colIndexed.filter(_._1 == 'O').map(_._2)
+//        val size = column.length
+//
+//        @tailrec
+//        def loop(
+//            currentIndex: Int,
+//            currentWalls: List[Int],
+//            currentRocks: List[Int],
+//            acc: List[Int]
+//        ): List[Int] =
+//          if (currentIndex == size)
+//            acc
+//          else {
+//            (currentWalls, currentRocks) match {
+//              case (_, Nil) =>
+//                acc
+//              case (Nil, _ :: _) =>
+//                currentRocks.indices.map(_ + currentIndex).toList ::: acc
+//              case (wall :: _, rock :: otherRocks) if wall > rock =>
+//                loop(currentIndex + 1, currentWalls, otherRocks, currentIndex :: acc)
+//              case (wall :: otherWalls, _ :: _) =>
+//                loop(wall + 1, otherWalls, currentRocks, acc)
+//            }
+//          }
+//
+//        val indices =
+//          if (walls.isEmpty)
+//            rocks.indices.toList
+//          else
+//            loop(LazyList.from(0).find(i => !walls.contains(i)).get, walls, rocks, Nil)
+//
+//        indices.map(size - _).sum
+//      }
 
-    val result1: List[Int] =
+    val originalInput = input.lines.toList
+
+    // (false: original, true: reversed), (false: original, true: reverse)
+    val directions: List[Boolean] =
+      List (false, true, false, )
+
+    def rollRocks(input: List[String], direction: Int): List[String] = {
+      val columns = input.transpose.map(line => if (directions(direction)) line.reverse else line)
+
       columns.map { column =>
         val colIndexed = column.zipWithIndex
         val walls = colIndexed.filter(_._1 == '#').map(_._2)
@@ -48,6 +94,7 @@ object Problem14 {
 
         indices.map(size - _).sum
       }
+    }
 
     println("First Part: " + result1.sum)
   }
