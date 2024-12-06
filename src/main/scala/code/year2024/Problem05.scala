fc6ce6c2deeb0e25a25566fa0d4825b16ca78ccc
Ricardo Martins <ricardocmartins91@gmail.com>
2024-12-06 10:15:24 +0000

    Added problem 05 of 2024 (part 2)

--- src/main/scala/code/year2024/Problem05.scala
+++ src/main/scala/code/year2024/Problem05.scala
@@ -2,8 +2,6 @@ package code.year2024
 
 import better.files.File
 
-import scala.annotation.tailrec
-
 object Problem05 {
 
   def main(args: Array[String]): Unit = {
@@ -25,27 +23,54 @@ object Problem05 {
     val checkLists: List[List[Int]] =
       inputLines.filter(_.contains(",")).map(_.split(",").toList.map(_.toInt))
 
-    val total: Int =
-      checkLists
-        .filter { list =>
-          list.tails.toList.forall {
-            case Nil | List(_) =>
-              true
-            case currentPage :: tail =>
-              tail.forall(afterPage =>
-                rules.get(afterPage) match {
-                  case None      => true
-                  case Some(seq) => !seq.contains(currentPage)
-                }
-              )
-          }
-        }
-        .map { list =>
-          list(list.size / 2)
-        }
-        .sum
+    def listIsValid(list: List[Int]): Boolean =
+      list.tails.toList.forall {
+        case Nil | List(_) =>
+          true
+        case currentPage :: tail =>
+          tail.forall(afterPage =>
+            rules.get(afterPage) match {
+              case None      => true
+              case Some(seq) => !seq.contains(currentPage)
+            }
+          )
+      }
 
-    println("First Part: " + total)
+    val validListsMiddlePart1: List[Int] =
+      checkLists
+        .filter(listIsValid)
+        .map(list => list(list.size / 2))
+
+    println("First Part: " + validListsMiddlePart1.sum)
+
+    def fixList(originalList: List[Int]): List[Int] =
+      originalList match {
+        case Nil | List(_) =>
+          originalList
+        case currentPage :: tail =>
+          val pageIndexToMoveOpt: Option[Int] =
+            tail.zipWithIndex
+              .find { case (afterPage, _) =>
+                rules.get(afterPage).exists(_.contains(currentPage))
+              }
+              .map(_._2)
+
+          pageIndexToMoveOpt match {
+            case None =>
+              currentPage :: fixList(tail)
+            case Some(pageIndexToMove) =>
+              val page = tail(pageIndexToMove)
+              fixList(page :: currentPage :: tail.filterNot(_ == page))
+          }
+      }
+
+    val validListsMiddlePart2: List[Int] =
+      checkLists
+        .filterNot(listIsValid)
+        .map(fixList)
+        .map(list => list(list.size / 2))
+
+    println("Second Part: " + validListsMiddlePart2.sum)
   }
 
 }
